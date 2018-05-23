225fb5f3154e37b54e8b87c4a5f3f47064ddef51
Dmitry Boulytchev <dboulytchev@gmail.com>
2018-05-23 16:40:46 +0300

    Fixed reify (really?)

--- IntrospectionWorkout.hs
+++ IntrospectionWorkout.hs
@@ -87,7 +87,7 @@ instance Product b => Product (a :|: b) where
   insert' (as :+: bs) (Left  a) = (as ++ [a]) :+: bs
   insert' (as :+: bs) (Right b) = as :+: (insert' bs b)
 
-reify :: (Product u, MultiPrj u u) => AppList u (Eithery u) -> Reify u
+reify :: (Product u, MultiPrj u u) => AppList u -> Reify u
 reify x = insert $ polymap multiPrj x
 
 
@@ -128,13 +128,13 @@ instance ComposeUniform fs b c => ComposeUniform (f :|: fs) b c where
 
 
 -- Type-discriminated application
-class ApplyUniform f a b where
+class ApplyUniform f a where
   applyUniform :: Uniform f b -> a -> b
 
-instance {-# OVERLAPPING #-} ApplyUniform (a :|: c) a b where
+instance {-# OVERLAPPING #-} ApplyUniform (a :|: c) a where
   applyUniform (f :+: _) x = f x
 
-instance  {-# OVERLAPPABLE #-} ApplyUniform y a b => ApplyUniform (x :|: y) a b where
+instance  {-# OVERLAPPABLE #-} ApplyUniform y a => ApplyUniform (x :|: y) a where
   applyUniform (_ :+: g) x = applyUniform g x
 
 --instance ApplyUniform (U a) a b where
@@ -162,13 +162,13 @@ type family Polyform u c = r | r -> u c where
 
 -- Type-discriminated application
 
-class ApplyPolyform f a b where
+class ApplyPolyform f a where
   applyPolyform :: Polyform f b -> a -> b -> a
 
-instance {-# OVERLAPPING #-} ApplyPolyform (a :|: c) a b where
+instance {-# OVERLAPPING #-} ApplyPolyform (a :|: c) a where
   applyPolyform (f :+: _) x b = f x b
 
-instance  {-# OVERLAPPABLE #-} ApplyPolyform y a b => ApplyPolyform (x :|: y) a b where
+instance  {-# OVERLAPPABLE #-} ApplyPolyform y a => ApplyPolyform (x :|: y) a where
   applyPolyform (_ :+: g) x b = applyPolyform g x b
 
 --instance ApplyPolyform (U a) a b where
@@ -188,29 +188,29 @@ instance {-# OVERLAPPABLE #-} Member a c => Member a (b :|: c) where
   prj = unsafeCoerce
 -}
 
-data AppList f c = Nil | forall a . (Show a, ApplyUniform f a c, ApplyPolyform f a c, ApplyTransform f a) => Cons a (AppList f c)
+data AppList f = Nil | forall a . (Show a, ApplyUniform f a, ApplyPolyform f a, ApplyTransform f a) => Cons a (AppList f)
 
-instance Show (AppList f c) where
+instance Show (AppList f) where
   show Nil = "[]"
   show (Cons h t) = show h ++ " : " ++ show t
 
-polymap :: Uniform f c -> AppList f c -> [c]
+polymap :: Uniform f c -> AppList f -> [c]
 polymap _ Nil = []
 polymap f (Cons h t) = applyUniform f h : polymap f t
 
-polyfoldl :: Uniform f (c -> c) -> AppList f (c -> c) -> c -> c
+polyfoldl :: Uniform f (c -> c) -> AppList f -> c -> c
 polyfoldl _  Nil       acc = acc
 polyfoldl f (Cons h t) acc = polyfoldl f t (applyUniform f h acc)
 
-polyfoldr :: Uniform f (c -> c) -> AppList f (c -> c) -> c -> c
+polyfoldr :: Uniform f (c -> c) -> AppList f -> c -> c
 polyfoldr _  Nil       acc = acc
 polyfoldr f (Cons h t) acc = applyUniform f h (polyfoldr f t acc)
 
-mapPolyForm :: Polyform f c -> AppList f c -> c -> AppList f c
+mapPolyForm :: Polyform f c -> AppList f -> c -> AppList f 
 mapPolyForm _ Nil _ = Nil
 mapPolyForm f (Cons h t) c = Cons (applyPolyform f h c) (mapPolyForm f t c)
 
-mapTransform :: Transform f -> AppList f c -> AppList f c
+mapTransform :: Transform f -> AppList f -> AppList f 
 mapTransform _ Nil = Nil
 mapTransform f (Cons h t) = Cons (applyTransform f h) (mapTransform f t)
 
@@ -227,7 +227,7 @@ main = do
 {-
   print $ polyfoldl ((\(x :: Int) acc -> x * acc :: Int) :+: (\x acc -> acc + length (x :: String) :: Int) :+: undefined) (Cons "b" (Cons (2 :: Int) (Cons "twitter" Nil))) 0
   print $ polyfoldr ((\(x :: Int) acc -> x * acc :: Int) :+: (\x acc -> acc + length (x :: String) :: Int) :+: undefined) (Cons "b" (Cons (2 :: Int) (Cons "twitter" Nil))) 0
--}
+
 --p :: ExList -> String
 --p Nil        = ""
 --p (Cons h t) = show h ++ p t
@@ -238,3 +238,4 @@ main = do
 --instance (ApplyUniformUniform b a b, List l b) => List (a :+: l) b where
   --polymap f (a :+: b) = applyUniform f a : polymap f b
 
+-}
