2673f8f9eef5cf6f8b47342d70ea101630ff1b1d
kajigor <kajigor@gmail.com>
2018-06-20 11:50:43 +0300

    Fold and freeVars

--- ExprDef.hs
+++ ExprDef.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE FlexibleContexts           #-}
 
 module ExprDef where
 
@@ -10,8 +11,8 @@ import Data.List ((\\), nub, concat, delete)
 import Data.Maybe
 import Unsafe.Coerce
 
-data Expr = Var String | Const Int | Bop String Expr Expr | Let Def Expr deriving Show
-data Def  = Def String Expr deriving Show
+data Expr = Var String | Const Int | Bop String Expr Expr | Let Def Expr deriving (Show, Eq)
+data Def  = Def String Expr deriving (Show, Eq)
 
 instance Term Expr where
   type Var Expr = String
@@ -30,12 +31,7 @@ instance Term Expr where
 
   make (Bop b _ _) subs = let ([l', r'] :+: [] :+: _) = reify subs in Bop b l' r'
   make (Let _ _  ) subs = let ([e'] :+: [d'] :+: _)  = reify subs in Let d' e'
-  make t           _                     = t
-
-rewriteE (Bop b l r) = Bop b r l
-rewriteE t = t
-
-rewriteD = id
+  make t           _    = t
 
 instance Term Def where
   type Var Def = String
@@ -49,13 +45,50 @@ instance Term Def where
 
   make (Def s _) subs = let ([e'] :+: [] :+: _) = reify subs in Def s e'
 
+flipBop = rewrite $ (\expr -> case expr of Bop b l r -> Bop b r l ; e -> e) :+: id :+: undefined
+
+simplBop = rewrite (simpl :+: id :+: undefined)
+  where simpl (Bop "+" (Const 0) r) = r
+        simpl (Bop "+" l (Const 0)) = l
+        simpl (Bop "*" (Const 1) r) = r
+        simpl (Bop "*" l (Const 1)) = l
+        simpl e                     = e
+
+sb expr = sb' expr (simplBop expr)
+  where sb' prev curr | prev == curr = curr
+        sb' _    curr = sb' curr (simplBop curr)
+
+fv :: Expr -> [Var Expr]
+fv e = fold (shallowFv :+: shallowFv :+: undefined) e []
+
+fv' e = fold makeFv e []
+
+foldish e = fold ((\e a -> case e of Var x -> x : a ; _ -> a) :+: (\ d a -> case d of Def x _ -> x `delete` a ) :+: undefined) e []
+
 test :: IO ()
 test =
   do
-
-    let t = Bop "+" (Var "a")  (Let (Def "b" (Bop "+" (Const 7) (Const 0))) (Bop "+" (Const 6) (Var "b")))
+    let t = Bop "+" (Var "a")  (Let (Def "b" (Bop "+" (Bop "*" (Const 1) (Const 7)) (Const 0))) (Bop "+" (Const 6) (Var "b")))
     print t
     putStrLn ""
 
-    print $ rewrite (rewriteE :+: rewriteD :+: undefined) t
+    print $ flipBop t
+    putStrLn ""
+
+    print $ simplBop t
+    putStrLn ""
+
+    print $ sb t
+    putStrLn ""
+
+    print $ foldish t
+    putStrLn ""
+
+    print $ fv t
+    putStrLn ""
+
+    print $ fv' t
+    putStrLn ""
+
+    print $ freeVars t
     putStrLn ""
--- MultiTerm.hs
+++ MultiTerm.hs
@@ -23,17 +23,25 @@ class Term t where
   type Var t :: *
   type Sub t :: *
 
-  var      :: t -> Maybe (Var t)
-  binder   :: t -> Maybe (Var t)
-  subterms :: t -> HeteroList (Sub t)
-  make     :: t -> HeteroList (Sub t) -> t
-  makeFV   :: (Eq (Var t)) => t -> [Var t] -> [Var t]
+  var       :: t -> Maybe (Var t)
+  binder    :: t -> Maybe (Var t)
+  subterms  :: t -> HeteroList (Sub t)
+  make      :: t -> HeteroList (Sub t) -> t
+  shallowFv :: (Eq (Var t)) => t -> [Var t] -> [Var t]
 
-  makeFV t a = case var t of
-                 Just  v -> v : a
-                 Nothing -> case binder t of
-                              Just v  -> filter (/= v) a
-                              Nothing -> a
+  shallowFv t a = case var t of
+                    Just  v -> v : a
+                    Nothing -> case binder t of
+                                 Just v  -> filter (/= v) a
+                                 Nothing -> a
+
+  freeVars :: ( Eq (Var t),
+                MakeFv (Sub t) (Var t),
+                LiftFold (Sub t) (Sub t) [Var t],
+                MakeFolder (Sub t) (Sub t) [Var t],
+                ApplyUniform (Sub t) t
+              ) => t -> [Var t]
+  freeVars e = fold makeFv e []
 
 type family ShallowRewriter t g = r | r -> t g where
   ShallowRewriter t g = Transform g -> Transform g -> t -> t
@@ -79,3 +87,58 @@ instance (Term t, LiftRewrite (Sub t) (Sub t), MakeRewriter (Sub t) (Sub t), App
                           (fs :: Transform (Sub t))
                           (shallow :: Transform (Sub t))
     in applyTransform fs t
+
+
+
+type family ShallowFolder t g c = r | r -> t g c where
+  ShallowFolder t g c = Uniform g (c -> c) -> Uniform g (c -> c) -> t -> c -> c
+
+class Term t => ShallowFold t c where
+  shallowFold :: ShallowFolder t (Sub t) c
+
+instance (Term t, ApplyUniform (Sub t) t) => ShallowFold t c where
+  shallowFold deep shallow t acc =
+    polyfoldr deep (subterms t) (applyUniform shallow t acc)
+
+class MakeFolder t g c where
+  makeFolder  :: LiftFolder t g c -> Uniform g (c -> c) -> Uniform g (c -> c) -> Uniform t (c -> c)
+
+instance MakeFolder U g c where
+  makeFolder _ _ _ = undefined
+
+instance (Term a, MakeFolder b g c) => MakeFolder (a :|: b) g c where
+  makeFolder (t :+: q) a b =
+    let f = t a b in
+    let g = makeFolder q a b in
+    f :+: g
+
+type family LiftFolder t g c = r | r -> t g c where
+  LiftFolder (a :|: b) g c = ShallowFolder a g c :+: LiftFolder b g c
+
+class LiftFold t g c where
+  liftFold :: LiftFolder t g c
+
+instance LiftFold U g c where
+  liftFold = undefined
+
+instance (Term a, g ~ Sub a, ApplyUniform g a, LiftFold b g c) => LiftFold (a :|: b) g c where
+  liftFold = shallowFold :+: liftFold
+
+class Term t => Fold t c where
+  fold :: Uniform (Sub t) (c -> c) -> t -> c -> c
+
+instance (Term t, LiftFold (Sub t) (Sub t) c, MakeFolder (Sub t) (Sub t) c, ApplyUniform (Sub t) t) => Fold t c where
+  fold shallow t acc =
+    let fs = makeFolder liftFold fs shallow
+    in applyUniform fs t acc
+
+
+
+class MakeFv u v where
+  makeFv :: Uniform u ([v] -> [v])
+
+instance MakeFv U v where
+  makeFv = undefined
+
+instance (Term a, v ~ Var a, Eq v, MakeFv b v) =>  MakeFv (a :|: b) v where
+  makeFv = shallowFv :+: makeFv
